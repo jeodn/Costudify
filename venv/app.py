efe56d43df969c378a331ac2aeb16708af0fd14d
josh <joshdrvillas@gmail.com>
2025-01-23 10:08:19 -0500

    move class definitions from app.py to modules.py

--- venv/app.py
+++ venv/app.py
@@ -3,98 +3,11 @@ This version of Costudify is the one where users can anonymously post their grou
 """
 
 from flask import Flask, render_template
-from modules import convert_to_dict, make_ordinal
+from modules import convert_to_dict, make_ordinal, get_posts
 from datetime import datetime
 
 app = Flask(__name__)
 application = app
-
-class User:
-    """
-    A user
-
-    === Attributes ===
-    username: username as string 
-    password: password as string
-    posts: list of all posts, posts stored as post ID
-
-
-    """
-    username: str
-    password: str
-    posts: list[int]
-    
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-        self.posts = []
-
-
-class Post:
-    """
-    A group chat posting
-
-    === Attributes ===
-    id: The post ID as a number.
-    date: date in unix time [OR MAYBE AS A STRING? IDK]
-    username: The post's user ID.
-    platform: The group chat's platform.
-    course: The group chat's course, if any. Can be empty string.
-    url: The URL to the group chat.
-    title: The title of the post.
-    description: The post text. 
-
-    === Representation Invariants ===
-
-    platform, course, url can all be empty string. 
-    """
-    id: int
-    date: str
-    user: User
-    platform: str
-    course: str
-    url: str
-    title: str
-    description: str
-
-    def __init__(self, id, date, user, platform, course, url, title, description):
-        self.id = id
-
-        self.date = date
-        #self.datestr = self.date.strftime("%Y/%m/%d %H:%M%S")
-
-        self.user = user
-        self.platform = platform
-        self.course = course
-        self.url = url
-        self.title = title
-        self.description = description
-
-    def get_app_icon(self):
-        """
-        Return filepath to icon of Post's platform.
-        """
-        app_icons = {"WeChat": "/static/wechat.png",
-                     "Instagram": "/static/instagram.png"
-                     }
-        
-        return app_icons[self.platform]
-
-
-# create a list of dicts from a CSV. Or, create a list of Posts.
-#posts_list = convert_to_dict("posts.csv")
-def get_posts(post_file: str):
-    """
-    Return a list of Post objects with information from posts.csv.
-    """
-    post_dicts = convert_to_dict("posts.csv") # list of dictionaries
-    out_list = []
-
-    for p in post_dicts:
-        out_list.append( Post(p['ID'], p['Date'], p['User'], p['Platform'], 
-                        p['Course'], p['URL'], p['Title'], p['Description']) )
-
-    return out_list
 
 posts_list = get_posts('posts.csv')
 print([post.title for post in posts_list])
